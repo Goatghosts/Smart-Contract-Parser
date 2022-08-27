e9920b041a6b5ddfbef6d741d614f9b61772986f
Александр <52637769+Goatghosts@users.noreply.github.com>
2022-08-27 22:40:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from selectolax.parser import HTMLParser
 import requests
 
+BLOCKCHAINS = {
+    "BSC": {
+        "url": "https://bscscan.com",
+    },
+    "ETHER": {
+        "url": "https://etherscan.io",
+    },
+    "POLYGON": {
+        "url": "https://polygonscan.com",
+    },
+    "MOON": {
+        "url": "https://moonscan.io",
+    },
+    "ARBI": {
+        "url": "https://arbiscan.io",
+    },
+}
+
 
 def get_headers(blockchain: str) -> dict:
+    referer = BLOCKCHAINS[blockchain]["url"]
     return {
-        "authority": f"{blockchain}",
         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
         "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
-        "referer": f"https://{blockchain}/",
+        "referer": f"{referer}/",
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
     }
 
@@ -20,8 +38,9 @@ def get_page(url: str, blockchain: str) -> str:
 def get_conracts(blockchain: str, all_pages: bool = True) -> dict:
     contracts = {}
     last_page = 6 if all_pages else 2
+    url = BLOCKCHAINS[blockchain]["url"]
     for page in range(1, last_page):
-        html = get_page(f"https://{blockchain}/contractsVerified/{page}?ps=100", blockchain)
+        html = get_page(f"{url}/contractsVerified/{page}?ps=100", blockchain)
         tree = HTMLParser(html)
         table_body = tree.css_first("tbody")
         rows = table_body.css("tr")
@@ -29,8 +48,10 @@ def get_conracts(blockchain: str, all_pages: bool = True) -> dict:
             try:
                 cols = row.css("td")
                 balance, currency = cols[4].text().split()
-                contracts[cols[0].text().strip()] = {
-                    "address": cols[0].text().strip(),
+                address = cols[0].text().strip()
+                contracts[address] = {
+                    "address": address,
+                    "url": f"{url}/address/{address}#code",
                     "name": cols[1].text().strip(),
                     "compiler": cols[2].text().strip(),
                     "version": cols[3].text().strip(),
@@ -45,16 +66,8 @@ def get_conracts(blockchain: str, all_pages: bool = True) -> dict:
     return contracts
 
 
-POOL = [
-    "etherscan.io",
-    "bscscan.com",
-    "polygonscan.com",
-    "moonscan.io",
-    "arbiscan.io",
-]
-
 if __name__ == "__main__":
-    contracts = get_conracts(POOL[0], True)
+    contracts = get_conracts("ARBI", True)
     for address, data in contracts.items():
-        print(data)
+        print(address, data["name"])
     print(len(contracts))
